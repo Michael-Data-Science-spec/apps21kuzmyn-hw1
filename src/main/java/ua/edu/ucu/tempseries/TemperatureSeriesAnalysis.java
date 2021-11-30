94e57c3ddd4bf40a92fce21ada129364f1fbfd89
Micahel-Data-Science-spec <mykuzmyn@gmail.com>
2021-11-30 20:47:40 +0200

    added functionality and testing

--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -1,52 +1,213 @@
 package ua.edu.ucu.tempseries;
 
+import java.util.InputMismatchException;
+import java.lang.IllegalArgumentException;
+
 public class TemperatureSeriesAnalysis {
 
-    public TemperatureSeriesAnalysis() {
+    private double[] temperatureSeries;
+    private int size = 1;
+    private int num;
 
+    public TemperatureSeriesAnalysis() {
+        double[] temperatureSeries = new double[size];
+        this.temperatureSeries = temperatureSeries;
     }
 
     public TemperatureSeriesAnalysis(double[] temperatureSeries) {
+        if (temperatureSeries.length == 0) {
+            throw new IllegalArgumentException();
+        }
 
+        num = temperatureSeries.length;
+
+        while (size < num) {
+            size = size * 2;
+        }
+
+        double[] arr = new double[size];
+        for (int i = 0; i < num; i++) {
+            if (temperatureSeries[i] < -273) {
+                throw new InputMismatchException();
+            }
+            arr[i] = temperatureSeries[i];
+        }
+
+        this.temperatureSeries = arr;
     }
 
     public double average() {
-        return -1;
+        if (this.temperatureSeries.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        else {
+            double avg = 0;
+            for (double d:this.temperatureSeries) {
+                avg += d;
+            }
+
+            avg = avg / this.num;
+            return avg;
+        }
     }
 
     public double deviation() {
-        return 0;
+        if (this.temperatureSeries.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            double avg = average();
+            double quad_sum = 0;
+            for (double d:this.temperatureSeries) {
+                quad_sum += Math.pow(d - avg, 2);
+            }
+            quad_sum = quad_sum / this.num;
+            quad_sum = Math.pow(quad_sum, 0.5);
+            return quad_sum;
+        }
     }
 
     public double min() {
-        return 0;
+        if (this.temperatureSeries.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            double min = Double.MAX_VALUE;
+            for (double d:this.temperatureSeries) {
+                if (d < min) {
+                    min = d;
+                }
+            }
+            return min;
+        }
     }
 
     public double max() {
-        return 0;
+        if (this.temperatureSeries.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            double max = -100000000000000000000.0;
+            for (double d:this.temperatureSeries) {
+                if (d > max) {
+                    max = d;
+                }
+            }
+            return max;
+        }
     }
 
     public double findTempClosestToZero() {
-        return 0;
+        if (this.temperatureSeries.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            double min_diff = Double.MAX_VALUE;
+            for (double d:this.temperatureSeries) {
+                if (Math.abs(d) < Math.abs(min_diff)) {
+                    min_diff = d;
+                }
+                else if (Math.abs(d) == Math.abs(min_diff) && d > min_diff) {
+                    min_diff = d;
+                }
+            }
+            return min_diff;
+        }
     }
 
     public double findTempClosestToValue(double tempValue) {
-        return 0;
+        if (this.temperatureSeries.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            double min_diff = Double.MAX_VALUE;
+            for (double d:this.temperatureSeries) {
+                if (Math.abs(d - tempValue) < Math.abs(min_diff - tempValue)) {
+                    min_diff = d;
+                }
+                else if (Math.abs(d - tempValue) == Math.abs(min_diff - tempValue) && d > min_diff) {
+                    min_diff = d;
+                }
+            }
+            return min_diff;
+        }
     }
 
     public double[] findTempsLessThen(double tempValue) {
-        return null;
+        if (this.temperatureSeries.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            double[] lessThan = new double[this.temperatureSeries.length];
+            int idx = 0;
+
+            for (double d:this.temperatureSeries) {
+                if (d < tempValue) {
+                    lessThan[idx] = d;
+                    idx += 1;
+                }
+            }
+            return lessThan;
+        }
     }
 
     public double[] findTempsGreaterThen(double tempValue) {
-        return null;
+        if (this.temperatureSeries.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            double[] greaterThan = new double[this.temperatureSeries.length];
+            int idx = 0;
+
+            for (double d:this.temperatureSeries) {
+                if (d >= tempValue) {
+                    greaterThan[idx] = d;
+                    idx += 1;
+                }
+            }
+            return greaterThan;
+        }
     }
 
     public TempSummaryStatistics summaryStatistics() {
-        return null;
+        double avgTemp = average();
+        double devTemp = deviation();
+        double minTemp = min();
+        double maxTemp = max();
+        TempSummaryStatistics tempSummaryStatistics = new TempSummaryStatistics(avgTemp, devTemp, minTemp, maxTemp);
+        return tempSummaryStatistics;
     }
 
     public int addTemps(double... temps) {
-        return 0;
+        int len = temps.length;
+        boolean changedSize = false;
+
+        for (double d:temps) {
+            if (d < -273) {
+                throw new InputMismatchException();
+            }
+        }
+
+        while (num + len > size) {
+            size = size * 2;
+            changedSize = true;
+        }
+
+        if (changedSize) {
+            double[] arr = new double[size];
+
+            for (int i = 0; i < num; i++) {
+                arr[i] = temperatureSeries[i];
+            }
+            this.temperatureSeries = arr;
+        }
+
+        for (double d:temps) {
+            temperatureSeries[num] = d;
+            num += 1;
+        }
+
+        return num;
     }
 }
--- src/test/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysisTest.java
+++ src/test/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysisTest.java
@@ -3,6 +3,8 @@ package ua.edu.ucu.tempseries;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.sql.SQLOutput;
+
 public class TemperatureSeriesAnalysisTest {
 
     @Test
@@ -38,6 +40,145 @@ public class TemperatureSeriesAnalysisTest {
         
         assertEquals(expResult, actualResult, 0.00001);        
     }
-    
 
+    @Test
+    public void testDevWithOneElementArray() {
+        // setup input data and expected result
+        double[] temperatureSeries = {-1.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        double expResult = 0.0;
+
+        // call tested method
+        double actualResult = seriesAnalysis.deviation();
+
+        // compare expected result with actual result
+        assertEquals(expResult, actualResult, 0.00001);
+    }
+
+    @Test
+    public void testDev() {
+        double[] temperatureSeries = {3.0, 1.0, 1.0, 3.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        double expResult = 1.0;
+
+        double actualResult = seriesAnalysis.deviation();
+
+        assertEquals(expResult, actualResult, 0.00001);
+    }
+
+    @Test
+    public void testMinWithOneElementArray() {
+        // setup input data and expected result
+        double[] temperatureSeries = {-1.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        double expResult = -1.0;
+
+        // call tested method
+        double actualResult = seriesAnalysis.min();
+
+        // compare expected result with actual result
+        assertEquals(expResult, actualResult, 0.00001);
+    }
+
+    @Test
+    public void testMin() {
+        double[] temperatureSeries = {3.0, -5.0, 1.0, 3.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        double expResult = -5.0;
+
+        double actualResult = seriesAnalysis.min();
+
+        assertEquals(expResult, actualResult, 0.00001);
+    }
+
+    @Test
+    public void testMaxWithOneElementArray() {
+        // setup input data and expected result
+        double[] temperatureSeries = {-1.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        double expResult = -1.0;
+
+        // call tested method
+        double actualResult = seriesAnalysis.max();
+
+        // compare expected result with actual result
+        assertEquals(expResult, actualResult, 0.00001);
+    }
+
+    @Test
+    public void testMax() {
+        double[] temperatureSeries = {3.0, -5.0, 1.0, 5.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        double expResult = 5.0;
+
+        double actualResult = seriesAnalysis.max();
+
+        assertEquals(expResult, actualResult, 0.00001);
+    }
+
+    @Test
+    public void testClosestToZero() {
+        double[] temperatureSeries = {3.0, -1.0, 1.0, 3.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        double expResult = 1.0;
+
+        double actualResult = seriesAnalysis.findTempClosestToZero();
+
+        assertEquals(expResult, actualResult, 0.00001);
+    }
+
+    @Test
+    public void testClosestToValue() {
+        double[] temperatureSeries = {3.0, -1.0, 1.0, 3.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        double expResult = 3.0;
+
+        double actualResult = seriesAnalysis.findTempClosestToValue(2.0);
+
+        assertEquals(expResult, actualResult, 0.00001);
+    }
+
+    @Test
+    public void testFindLessThan() {
+        double[] temperatureSeries = {-3.0, 1.0, -1.0, 3.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        double[] expResult = {-3.0, 1.0, -1.0};
+
+        double[] actualResult = seriesAnalysis.findTempsLessThen(2.0);
+
+        for (int i = 0; i < expResult.length; i++) {
+            assertEquals(expResult[i], actualResult[i], 0.00001);
+        }
+    }
+
+    @Test
+    public void testFindGreaterThan() {
+        double[] temperatureSeries = {-3.0, 1.0, -1.0, 3.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        double[] expResult = {1.0, 3.0};
+
+        double[] actualResult = seriesAnalysis.findTempsGreaterThen(1.0);
+
+        for (int i = 0; i < expResult.length; i++) {
+            assertEquals(expResult[i], actualResult[i], 0.00001);
+        }
+    }
+
+    @Test
+    public void testSummaryStatistics() {
+        double[] temperatureSeries = {3.0, -5.0, 1.0, 5.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+
+        double avg = 1.0;
+        double dev = 3.74165;
+        double min = -5.0;
+        double max = 5.0;
+
+        TempSummaryStatistics actualResult = seriesAnalysis.summaryStatistics();
+
+        assertEquals(avg, actualResult.getAvgTemp(), 0.00001);
+        assertEquals(max, actualResult.getMaxTemp(), 0.00001);
+        assertEquals(min, actualResult.getMinTemp(), 0.00001);
+        assertEquals(dev, actualResult.getDevTemp(), 0.00001);
+    }
 }
